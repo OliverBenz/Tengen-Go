bf4be6cc7b65cddfe2ec016120d6f74463e58aff
Oliver Benz <oliver.benz@outlook.com>
2026-03-08 18:40:53 +0100

    BoardViewer: New tool to render board from different formats.

---
+++ tools/boardViewer/main.cpp
@@ -0,0 +1,23 @@
+#include "core/serializer.hpp"
+#include "gui/boardWidget.hpp"
+
+#include <QApplication>
+#include <QShortcut>
+
+int main(int argc, char* argv[]) {
+	QApplication application(argc, argv);
+
+	tengen::Board board(9u);
+	if (!tengen::internal::readTestBoard(std::filesystem::path{PATH_TEST_IMG}/"example.txt", board)) {
+		return -1;
+	}
+
+	tengen::gui::BoardWidget boardWidget(board);
+	boardWidget.resize(800, 800);
+
+	QShortcut escShortcut(QKeySequence(Qt::Key_Escape), &boardWidget);
+	QObject::connect(&escShortcut, &QShortcut::activated, &application, &QApplication::quit);
+
+	boardWidget.show();
+	return application.exec();
+}
