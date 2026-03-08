d9381e8023771a2d6e1e7250a6e6e6fe5df0f038
Oliver Benz <oliver.benz@outlook.com>
2026-03-08 18:40:26 +0100

    Perception.GTest: Move TXT file reader to core.

--- src/game/core/include/core/serializer.hpp
+++ src/game/core/include/core/serializer.hpp
@@ -4,9 +4,12 @@
 
 #include <filesystem>
 
-namespace tengen::vision {
-namespace gtest {
+namespace tengen {
 
+
+namespace internal {
+
+// TODO: Rename and structure upgrade once we do more. Currently only support reading TXT .BW format.
 /*! Read a board state from a test file.
  *  \param [in]  testFile The TXT file containing the board information in .BW format.
  *  \param [out] board    Board information as specified in the file.
