9a25a43b8abe2c3e1fdef37cd99aa9f64194c8fd
Oliver Benz <oliver.benz@outlook.com>
2026-03-21 21:40:34 +0100

    Nix: Add lld support.

--- flake.nix
+++ flake.nix
@@ -9,29 +9,35 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages =
-          let
-            opencvWithGui = pkgs.opencv4.override {
-              enableGtk3 = true;
-            };
-          in [
+      devShells.${system}.default =
+        let
+          opencvWithGui = pkgs.opencv4.override {
+            enableGtk3 = true;
+          };
+          runtimeLibraries = [
+            pkgs.qt6.qtbase
+            pkgs.qt6.qtwayland
+            pkgs.glfw
+            opencvWithGui
+            pkgs.gtk3
+            pkgs.zlib
+            pkgs.libpng
+            pkgs.libjpeg
+            pkgs.libtiff
+            pkgs.libwebp
+            pkgs.libglvnd
+            pkgs.stdenv.cc.cc.lib
+          ];
+        in pkgs.mkShell {
+        packages = [
           pkgs.gcc
           pkgs.cmake
-          pkgs.qt6.qtbase
-          pkgs.qt6.qtwayland
-          pkgs.glfw
-          opencvWithGui
-          pkgs.gtk3
           pkgs.pkg-config
-          pkgs.zlib
-          pkgs.libpng
-          pkgs.libjpeg
-          pkgs.libtiff
-          pkgs.libwebp
-        ];
+        ] ++ runtimeLibraries;
 
         shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibraries}:$LD_LIBRARY_PATH"
+
           echo "Dev shell ready. Use 'go-ui' to run the GUI application on x64 Debug."
 
           alias go-ui='nixGL  ./out/bin/x64/Debug/goUi'
