933c4150aa3a04e5bd681fc489827af14a7a3fe3
Oliver Benz <oliver.benz@outlook.com>
2026-05-19 21:24:16 +0200

    Runtime: Add open game session.

---
+++ src/game/runtime/include/tengen/openSession.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "core/IGameStateListener.hpp"
+#include "core/game.hpp"
+#include "tengen/IGameSession.hpp"
+#include "tengen/eventHub.hpp"
+
+#include <mutex>
+#include <thread>
+
+namespace tengen::app {
+
+//! Free play locally you control both players.
+class OpenSession : public IGameSession, public IGameStateListener {
+public:
+	OpenSession(std::size_t boardSize);
+	~OpenSession() override;
+
+	// GameSession Interface
+	GameStatus status() const override;
+	Board board() const override;
+	Player currentPlayer() const override;
+
+	void tryPlace(unsigned x, unsigned y) override;
+	void tryPass() override;
+	void tryResign() override;
+	void shutdown() override;
+
+	// AppSignal Handlers
+	void subscribe(app::IAppSignalListener* listener, uint64_t mask) override;
+	void unsubscribe(app::IAppSignalListener* listener) override;
+
+	// Game Handlers
+	void onGameDelta(const GameDelta& delta) override;
+
+private:
+	Game m_game;
+	EventHub m_eventHub;
+
+	std::thread m_gameThread; //!< Runs the game loop.
+	mutable std::mutex m_stateMutex;
+
+	// TODO: Position class should not be server dependent.
+	//       Then we can reuse it here.
+	// Game State
+	Board m_board;
+	Player m_currentPlayer{Player::Black};
+	GameStatus m_status{GameStatus::Active};
+};
+
+} // namespace tengen::app
